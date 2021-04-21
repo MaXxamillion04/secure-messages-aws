38a5f9a6746620ba45e4dd658a2fb2a8277fb936
root <maxxamillion04@gmail.com>
2021-04-21 14:31:30 -0500

    changed signout button

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import { AmplifySignOut } from 'aws-amplify-react'
+//import { AmplifySignOut } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
 
 
 export default function Header(props) {
+
+    const signOut = () =>{
+        Auth.signOut()
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
 
 return (<div className="HeaderSection">
 <Grid item xs={8}>
     props.userName
 </Grid>
 <Grid item xs={4}>
-    <AmplifySignOut />
+    <Button class="SignOutButton" onClick={this.signOut} >Sign Out</Button>
 </Grid>
 </div>
 );
