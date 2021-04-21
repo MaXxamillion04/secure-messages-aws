de903fcd376dacbfd59848e98e317857b7e574b7
root <maxxamillion04@gmail.com>
2021-04-21 14:58:29 -0500

    fixed header components

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 //import { AmplifySignOut } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-
+import Amplify, { Auth } from "aws-amplify";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 export default function Header(props) {
+  const userName = Auth.currentUserInfo().userName;
 
-    const signOut = () =>{
-        Auth.signOut()
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-    }
+  const signOut = () => {
+    Auth.signOut()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
 
-return (<div className="HeaderSection">
-<Grid item xs={8}>
-    props.userName
-</Grid>
-<Grid item xs={4}>
-    <Button class="SignOutButton" onClick={signOut} >Sign Out</Button>
-</Grid>
-</div>
-);
-
+  return (
+    <div className="HeaderSection">
+      <Grid container direction="row" spacing={1}>
+        <Grid item xs={8}>
+          <span className="UserName">userName</span>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            variant="contained"
+            color="secondary"
+            class="SignOutButton"
+            onClick={signOut}
+          >
+            Sign Out
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
